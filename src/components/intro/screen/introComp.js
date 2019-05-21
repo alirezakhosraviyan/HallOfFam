717fe3cdd813caf703fbf7410e05a5373dd46e30
alireza khosraviyan <alireza.infotech@gmail.com>
2019-05-21 15:30:23 +0430

    intro flow navigation updated

--- src/components/intro/screen/introComp.js
+++ src/components/intro/screen/introComp.js
@@ -2,11 +2,9 @@ import React from 'react';
 import AppIntroSlider from 'react-native-app-intro-slider';
 import slides from '../utils/slides';
 
-export default (props) => (
-    <AppIntroSlider
-        slides={slides}
-        onDone={()=>props.navigation.navigate('welcome')}
-        showSkipButton={true}
-        onSkip={()=>props.navigation.navigate('welcome')}
-    />
-);
+export default (props) => (<AppIntroSlider
+    slides={slides}
+    onDone={() => props.navigation.navigate('mainStack')}
+    showSkipButton={true}
+    onSkip={() => props.navigation.navigate('mainStack')}
+/>);
